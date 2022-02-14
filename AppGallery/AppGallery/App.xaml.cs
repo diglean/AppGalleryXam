88d1f3e976e47709a024ea1417987c48f200937d
diglean <diegoleandro2002@gmail.com>
2022-02-14 11:59:24 -0300

    CarouselPage using C# and XAML

--- AppGallery/AppGallery/App.xaml.cs
+++ AppGallery/AppGallery/App.xaml.cs
@@ -10,10 +10,22 @@ namespace AppGallery
         {
             InitializeComponent();
 
+            #region NavigationPage
             //var pagina = new NavigationPage(new XamarinForms.Paginas.PaginaDeNavegacao.Conteudo01());
             //MainPage = pagina;
-
-            MainPage = new XamarinForms.Paginas.Modal.Conteudo01();
+            #endregion
+            #region Modal
+            //MainPage = new XamarinForms.Paginas.Modal.Conteudo01(); MODAL
+            #endregion
+            #region Carrossel C#
+            //var pagina = new CarouselPage(); !-!-!CARROSSEL EM C#!-!-!
+            //pagina.Children.Add(new XamarinForms.PaginaDeCarrossel.Conteudo01());
+            //pagina.Children.Add(new XamarinForms.PaginaDeCarrossel.Conteudo02());
+            //pagina.Children.Add(new XamarinForms.PaginaDeCarrossel.Conteudo03());
+            //pagina.CurrentPage = pagina.Children[1];
+            //MainPage = pagina;
+            #endregion
+            MainPage = new XamarinForms.PaginaDeCarrossel.Carrossel();
         }
 
         protected override void OnStart()
