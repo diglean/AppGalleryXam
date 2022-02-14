59128f74f9864301644ba5fa6163e699e60179d6
diglean <diegoleandro2002@gmail.com>
2022-02-14 11:26:06 -0300

    SplashScreen, NavigationPage, InsertBeforePage and RemovePage

---
+++ AppGallery/AppGallery/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppGallery
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var pagina = new NavigationPage(new XamarinForms.Paginas.PaginaDeNavegacao.Conteudo01());
+            MainPage = pagina;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
