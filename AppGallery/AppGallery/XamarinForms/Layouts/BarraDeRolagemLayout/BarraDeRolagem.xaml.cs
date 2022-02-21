ee11f163ac355bdc9c6684d51ea0d23f9f97f3a5
diglean <diegoleandro2002@gmail.com>
2022-02-21 15:20:57 -0300

    ScrollView - ScrollX & ScrollY

--- AppGallery/AppGallery/XamarinForms/Layouts/BarraDeRolagemLayout/BarraDeRolagem.xaml.cs
+++ AppGallery/AppGallery/XamarinForms/Layouts/BarraDeRolagemLayout/BarraDeRolagem.xaml.cs
@@ -21,5 +21,10 @@ namespace AppGallery.XamarinForms.Layouts.BarraDeRolagemLayout
         {
             BarraDeRolage.ScrollToAsync(SegundaLabel, ScrollToPosition.Start, true);
         }
+
+        private void ScrollView_Scrolled(object sender, ScrolledEventArgs e)
+        {
+            posicao.Text = e.ScrollX + " - " + e.ScrollY;
+        }
     }
 }
